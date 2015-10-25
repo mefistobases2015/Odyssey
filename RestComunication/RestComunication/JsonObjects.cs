e0d82cea391b50e22ef597447b99ff5fdc02bb10
Brais <abcbrais@gmail.com>
2015-10-25 12:23:30 -0600

    Se solucionó un problema de recursividad en el JSON por lo que en los objetos para parsear JSON's no se necesita los arreglos de integridad referencial. Estos fueron eliminados de los objetos de parsear JSON's.

--- RestComunication/RestComunication/JsonObjects.cs
+++ RestComunication/RestComunication/JsonObjects.cs
@@ -12,8 +12,35 @@ namespace RestComunication
 
     public class Credentials
     {
-        public string[] propiedades_tbl { set; get; }
         public string user_name { set; get; }
         public string pass { set; get; }
+    }
+
+    public class Songs
+    {
+        public string song_id { set; get; }
+        public string metadata_id { set; get; }
+        public string song_directory { set; get; }
+
+    }
+
+    public class Versions
+    {
+        public string version_id { set; get; }
+        public string song_id { set; get; }
+        public string submission_date { set; get; }
+        public string id3v2_title { set; get; }
+        public string id3v2_author { set; get; }
+        public string id3v2_lyrics { set; get; }
+        public string id3v2_album { set; get; }
+        public string id3v2_genre { set; get; }
+        public string id3v2_year { set; get; }
+    }
+
+    public class Properties
+    {
+        public string user_name { set; get; }
+        public string song_id { set; get; }
+        public string song_name { set; get; }
     }
 }
