08c991b085ac2ae19ad5a2ed4ed1a957548ecc49
Brais <abcbrais@gmail.com>
2015-11-06 03:28:06 -0600

    Programa para crear una base de datos en sql server y además agregarle las tablas.

---
+++ DatabaseTrial/DatabaseTrial/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace DatabaseTrial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sql_path = "../../../OdysseyOffline.sql";
+
+            string connec_str = "Server=localhost;Integrated security=SSPI;database=master";
+
+            string connec_str2 = "Server=localhost;Integrated security=SSPI;database=OdysseyOffline";
+
+            string script = File.ReadAllText(sql_path);
+
+            using (SqlConnection conn = new SqlConnection(connec_str))
+            {
+                SqlCommand command = new SqlCommand("CREATE DATABASE OdysseyOffline", conn);
+                try
+                {
+                    conn.Open();
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine("Todo salio bien");
+
+                    conn.Close();
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+
+
+            using (SqlConnection conn = new SqlConnection(connec_str2))
+            {
+                SqlCommand command = new SqlCommand(script, conn);
+                try
+                {
+                    conn.Open();
+                    command.ExecuteNonQuery();
+
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+
+            Console.Read();
+        }
+    }
+}
